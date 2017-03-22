e6ff9d509686331754638b9f3f9437751841c168
Sean McGrath <Sean McGrath>
2017-03-22 19:58:08 +0000

    Added the a function which creates a stack

--- countDown.rkt
+++ countDown.rkt
@@ -9,3 +9,18 @@ myNumber
 
 ;The list of numbers used to calculate the random target.
 (define numbers(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+
+
+
+;this function creates a stack which we will use to push and pop items from the list.
+;It was taken from: http://stackoverflow.com/questions/29244677/implementation-of-lifo-list-in-scheme
+(define (make-stack)
+  (let ((stack '()))
+    (lambda (msg . args)
+      (cond 
+        [(eq? msg 'pop!)  (set! stack (cdr stack))]
+        [(eq? msg 'push!) (set! stack (append (reverse args) stack))]
+        [(eq? msg 'stack) stack]
+        [else "Not valid message!"]))))
+
+
--- numberAndOperatorsCombinations.rkt
+++ numberAndOperatorsCombinations.rkt
@@ -1,1 +1,12 @@
 #lang racket
+
+;this function creates all of the possible combinations of the list 
+(define all-nums
+  (permutations (list 100 50 25 10 2 1)))
+
+(define ops (list '+ '- '* '/))
+
+(define all-ops (cartesian-product ops ops ops ops))
+
+all-ops
+;all-nums
