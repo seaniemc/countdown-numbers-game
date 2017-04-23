b4c6550dcbdc98f86fb783dc6f0d2848afe9f344
Sean McGrath <Sean McGrath>
2017-04-23 14:26:02 +0100

    updated rpnexample

--- possibleSolution.rkt
+++ possibleSolution.rkt
@@ -36,7 +36,7 @@
                [l (combine p)]
                #:when (= target (eval-expr l)))
      l)))
- (solve 10 10 10 5 200)|#
+ (solve 10 10 10 5 200)
 
 (require srfi/1)
 (require racket/generator)
@@ -106,4 +106,52 @@
                #:when (= target (eval-expr l)))
      l)))
  
-(pretty-print (solve 6 6 5 2 17))
+(pretty-print (solve 6 6 5 2 17))|#
+
+(require srfi/1)
+
+(define ops '(+ - * /))
+
+(define (combine4 n1 n2 n3 n4)
+  (concatenate
+   (map (lambda (op)
+          (map (lambda (e) `(,op ,n4 ,e)) (combine3 n1 n2 n3)))
+        ops)))
+
+(define (combine3 n1 n2 n3)
+  (concatenate
+   (map (lambda (op)
+          (map (lambda (e) `(,op ,n3 ,e)) (combine2 n1 n2)))
+        ops)))
+
+(define (combine2 n1 n2)
+  (map (lambda (op) `(,op ,n1 ,n2)) ops))
+
+(define (eval-expr e)
+  (with-handlers ([exn:fail:contract:divide-by-zero?
+                   (lambda (exn) +inf.0)])
+    (eval e)))
+
+(define solve
+  (lambda (n1 n2 n3 n4 target)
+    (let* ([perms (permutations `(,n1 ,n2 ,n3 ,n4))]
+           [expr-lists (map (lambda (perm)
+                              (combine4 (first perm)
+                                        (second perm)
+                                        (third perm)
+                                        (fourth perm)))
+                            perms)]
+           [val-lists (map (lambda (expr-list)
+                             (map eval-expr expr-list)) expr-lists)]
+           ;; For each perm, see if there's a val in its val-list that is equal to target.
+           ;; If so, hold on to the corresponding expr from its expr-list.
+           [solutions (filter-map (lambda (perm expr-list val-list)
+                                    (let ([idx (list-index (lambda (elem)
+                                                             (equal? elem target)) val-list)])
+                                      (if idx
+                                          (list-ref expr-list idx)
+                                          #f)))
+                                  perms expr-lists val-lists)])
+      (delete-duplicates solutions))))
+
+(solve 6 6 5 2 17)
