f32f8cd6cb3b4cd3775705380ea3d8c9d71ed3be
Sean McGrath <Sean McGrath>
2017-04-24 02:44:03 +0100

    updated countDown.rkt to outline the steps needed to finish the program

--- countDown.rkt
+++ countDown.rkt
@@ -19,13 +19,13 @@
 ;this is a sample list of 6 numbers
 (define all-nums (list 100 50 25 10 2 1))
 ;contains 720 posible unique combinations
- (length(remove-duplicates (permutations all-nums)))
+ ;(length(remove-duplicates (permutations all-nums)))
 
 ; this create a list of all numbers and operators possible
 (define main-list (append all-nums ops-list))
 
 ;a function which creates all the possible permutations which turns out to be 39916800
-(define noDups (remove-duplicates (permutations main-list)))
+;(define noDups (remove-duplicates (permutations main-list)))
 
 
 ;This function can be used to calculate all instances of valid rpn.
@@ -44,8 +44,9 @@
                    (reverse (cons x s)))])))
 
 
-;(calculate-RPN '(20 50 * 6 1 * 5 - 2 3 * +))
+(calculate-RPN '(20 50 * 6 1 * 5 - 2 3 * +))
 
+;this function gives a true or false if a list is valid rpn
 (define (valid-rpn? e [s 0])
     ; if e is null
     (if(null? e)
@@ -61,7 +62,23 @@
            (valid-rpn? (cdr e) (- s 1))           
            #f))))
 
-(valid-rpn?  (list 100 50 * 6 1 * 5 - 2 3 * +))
+(valid-rpn?  (list 100 50 * 20 52 * + ))
+(valid-rpn?  (list + 50 * 20 52 * + ))
+
+#|
+========The steps I belive which are needed to get the program to work.==========
+The program can generate a random taget number.
+We can generate all the permutations and combinations of 6 numbers and 5 operators.
+> We have a function which evaluates rpn and have a function which will check to see if a list is valid rpn.
+> So we need to create a function which passes in the target number and a list of lists containing rpn.
+> This function must first check to see if the current list is valid rpn. If it valid rpn then
+> the function will keep evaluating the lists and checking each out come against the target to see if
+> it is correct. Once it has found the correct answer the program ends.
+
+> To increase the performance of the algorithim, cutting down on the number of redudant permutations
+> would be the first step. We would need to remove all permutations which are not valid rpn.
+> This can be done through a combination of currying and careasn-product
+|#
 
 
 
